6a5e9c7bda073db3aa0efcf010628bc0df7c9105
Guillaume Marques <guillaume.marques@protonmail.com>
2025-08-09 22:46:09 +0200

    bug in reduced cost computation

--- src/ColGen/reduced_costs.jl
+++ src/ColGen/reduced_costs.jl
@@ -7,11 +7,10 @@ struct ReducedCosts
 end
 
 _constr_sign(::Type{MOI.ConstraintIndex{F, S}}) where {F,S <: MOI.GreaterThan{Float64}} = 1.0
-_constr_sign(::Type{MOI.ConstraintIndex{F, S}}) where {F,S <: MOI.LessThan{Float64}} = -1.0
+_constr_sign(::Type{MOI.ConstraintIndex{F, S}}) where {F,S <: MOI.LessThan{Float64}} = 1.0
 _constr_sign(::Type{MOI.ConstraintIndex{F, S}}) where {F,S <: MOI.EqualTo{Float64}} = 1.0
 
 function compute_reduced_costs!(context::DantzigWolfeColGenImpl, phase::MixedPhase1and2, mast_dual_sol::MasterDualSolution)
-    obj_sign = is_minimization(context) ? 1.0 : -1.0
     reduced_costs_dict = Dict{Any,Dict{MOI.VariableIndex,Float64}}()
 
     for (sp_id, pricing_sp) in get_pricing_subprobs(context)
@@ -34,7 +33,7 @@ function compute_reduced_costs!(context::DantzigWolfeColGenImpl, phase::MixedPha
                     constr_sign = _constr_sign(constraint_type)
                     if haskey(constraint_dict, constraint_value)
                         dual_value = constraint_dict[constraint_value]
-                        dual_contribution += obj_sign * constr_sign * coeff * dual_value
+                        dual_contribution += constr_sign * coeff * dual_value
                     end
                 end
             end
